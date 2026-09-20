6e7b4567461ee72cbc81a55f589e40aa87147bb7
Chirag Chauhan <crc442@nyu.edu>
2026-09-20 17:07:03 -0400

    core: NDJSON journal with round-trip and append tests

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,5 @@ export * from "./types";
 export { jevSystemOne, DEFAULT_JEV_MODEL } from "./providers/jev";
 export { mockSystemOne, noulAnswer, choiceAnswer, scoreAnswer } from "./providers/mock";
 export type { MockSystemOne } from "./providers/mock";
+export { openJournal, appendJournalEvent, loadJournal } from "./journal";
+export type { Journal, JournalEvent } from "./journal";
